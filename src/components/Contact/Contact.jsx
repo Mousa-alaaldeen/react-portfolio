950fe8e97ad211ffa7d9ebc4bfb242fd1127c92b
Mousa-alaaldeen <azmimousa396@gmail.com>
2024-12-14 12:06:49 +0300

    first commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:mousa.alaaldeen2000@gmail.com">mousa.alaaldeen2000@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href=" https://www.linkedin.com/in/mousa-alaaldeen">linkedin.com/in/mousa-alaaldeen </a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/Mousa-alaaldeen">github.com/Mousa-alaaldeen</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
